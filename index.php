4cf2e75ef2c1cd1e13894216de81fb2b99a62894
Aluno <Aluno@10.1.20.45>
2015-11-30 21:08:53 -0200

    closes #5. closes #6

--- index.php
+++ index.php
@@ -135,10 +135,10 @@
 
         <section id="noticias" class="container-fluid">
             <h1>Notícias</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            
+                <?php include 'noticias.php' ?>
         </section>
-
+        
         <section id="fotos" class="container-fluid">
             <h1>Fotos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
---
+++ noticias.php
@@ -0,0 +1,50 @@
+<!-- Trigger the modal with a button -->
+<div clas="row">
+    <div class="span4">
+        <h2>Edital SESI SENAI de inovação</h2>
+        <p>Inovar é fundamental para a competitividade da indústria brasileira. O Edital 
+            SENAI SESI de Inovação é uma iniciativa que valoriza essa prática, promovendo o 
+            desenvolvimento de novos produtos, processos e serviços inovadores.</p>
+        
+        <button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Abrir Notícia</button>
+
+<!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+    <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+        <!--<button type="button" class="close" data-dismiss="modal">&times;</button>-->
+        
+            <div class="panel panel-success">
+                <div class="panel-heading">Edital SESI SENAI de inovação</div>
+            <div class="panel-body">Inovar é fundamental para a competitividade da indústria brasileira. O Edital 
+            SENAI SESI de Inovação é uma iniciativa que valoriza essa prática, promovendo o 
+            desenvolvimento de novos produtos, processos e serviços inovadores. 
+            Na edição de 2015, busca-se reforçar o apoio a projetos elaborados em redes 
+            (multidisciplinares), com empresas industriais brasileiras, com startups e empresas 
+            de base tecnológica. Tais projetos receberão aporte de recursos não reembolsáveis 
+            para o desenvolvimento de propostas inovadoras.
+            HISTÓRICO - O Edital de Inovação foi lançado em 2004 e, desde o seu lançamento, 
+            foram aprovados 600 projetos em parceria com 552 empresas industriais brasileiras 
+            com investimentos de mais de R$ 117 milhões.</div>
+            </div>
+        
+        <!--<h4 class="modal-title"></h4>
+      </div>
+      <div class="modal-body" id="modal-body">
+        <p></p>
+      </div>-->
+            <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+            </div>
+        <div class="modal-dialog modal-lg"/></div>
+
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+
