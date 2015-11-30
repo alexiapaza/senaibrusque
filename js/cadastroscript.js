cf97350cc6c44a4b3f851b1dbbdd8f0ee2f0e266
Alexia <alexiapaza>
2015-11-30 18:51:39 -0200

    closes #5. closes #6

--- js/cadastroscript.js
+++ js/cadastroscript.js
@@ -1,7 +1,62 @@
-$("#formcadastro") .validator().on ("submit", function (event) {
-    if
-}
+$("#formCadastro").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // Formulário inválido
+        cadastroError();
+        submitCadastroMSG(false, "Por favor preencha corretamente com o seu e-mail.");
+    } else {
+        // Tudo azul!
+        event.preventDefault();
+        submitCadastro();
+    }
+});
 
-function cadastroError () {
+function submitCadastro() {
+    // Inicializa variáveis com o conteúdo do formulário
+    
+    var email = $("#cadastroEmail").val();
+    
+    console.log("email=" + email);
+
+    /**
+     * cadastro.php
+     * Cadastro no Banco de dados e envia o e-mail
+     */
+    $.ajax({
+        type: "POST",
+        url: "cadastro/cadastro.php",
+        data: "email=" + email,
+        success: function (text) {
+            if (text == "success") {
+                console.log("cadastroSuccess");
+                cadastroSuccess();
+            } else {
+                console.log("cadastroSuccess FALHOU. Texto: " + text);
+            }
+        }
+    });
+}
+function cadastroSuccess() {
+    //Limpa o formulário
+    $("#formCadastro")[0].reset();
+    //Envia mensagem de sucesso!
+    $("#formCadastro").addClass('hidden');
+    submitCadastroMSG(true, "Email Cadastrado!");
     
 }
+
+function submitCadastroMSG(valid, msg) {
+    var msgClasses;
+    if (valid) {
+        msgClasses = "h3 text-center tada animated text-cadastro";
+    } else {
+        msgClasses = "h3 text-center rubberBand animated text-danger";
+    }
+    $("#msgCadastroSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+function cadastroError() {
+    $("#formCadastro").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $(this).removeClass();
+    });
+}
+
